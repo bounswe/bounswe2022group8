28050c052992d99750d94e11c4804d031211701a
Karahan Sarıtaş <44376034+KarahanS@users.noreply.github.com>
2022-05-07 17:28:11 +0300

    added Django rest framework to the settings, updated requirements, added urls and views

--- Practice App/api/urls.py
+++ Practice App/api/urls.py
@@ -6,7 +6,19 @@ Function views
 
 from django.urls import path
 from .views import views
+from .views import view_TVSeries
+
+# practice_home : pattern that matches with the empty route
+# That pattern will be handled by the function views.home(request).
+# It returns an HttpResponse.
+
+# http://127.0.0.1:8000/practice --> practice_home  (HTTP Response)
+# http://127.0.0.1:8000/practice/about --> about_section  (HTTP Response)
+# http://127.0.0.1:8000/practice/api_home --> REST API  (JSON Response) 
 
 urlpatterns = [
     path('', views.home, name = "practice_home"),
+    path('about/', views.about, name = "about_section"),
+    path('api_home/', views.api_home, name= "rest_api"),
+    # path('episodes/<str:series>', view_TVSeries.episodes, name="episode_list")
 ]
--- Practice App/api/views/views.py
+++ Practice App/api/views/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import JsonResponse 
 
 # Create your views here.
 
+# Using HttpResponse for debug, we should use JsonResponse
+# since this is a REST API project.
+
 def home(request):
     return HttpResponse("<h1>Practice App Home</h1>")
+
+def about(request):
+    return HttpResponse("<h1>About Section</h1>")
+
+def api_home(request): 
+    data = {}
+    data['params'] = dict(request.GET)  # query parameters
+    data['headers'] = dict(request.headers)
+    data['content_type'] = request.content_type
+    return JsonResponse(data)
