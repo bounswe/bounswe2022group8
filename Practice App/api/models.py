af0d104bfc22388d8906b1a283d301567e5ffbf2
Karahan Sarıtaş <44376034+KarahanS@users.noreply.github.com>
2022-05-11 14:44:05 +0300

    made follower/follow fields 'not required', added description for tag, added __str__ functions for all

--- Practice App/api/models.py
+++ Practice App/api/models.py
@@ -11,33 +11,44 @@ class myUser(models.Model):
     name = models.CharField(max_length=100)
     surname = models.CharField(max_length=100)
     email = models.EmailField(unique=True, max_length=254)  # EmailField is a CharField that checks the value for a valid email address using EmailValidator
-    followers = models.ManyToManyField(User, related_name= "followed_by")
-    followed_users = models.ManyToManyField(User, related_name= "follows")
+    followers = models.ManyToManyField(User, related_name= "followed_by", blank=True)   # not required
+    followed_users = models.ManyToManyField(User, related_name= "follows", blank=True)  # not required
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at =models.DateTimeField(auto_now=True)
 
+
+    def __str__(self):
+        return self.name + " " + self.surname 
 
 class Tag(models.Model):
     tagname = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)  # description about the tag
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at =models.DateTimeField(auto_now=True)
 
-class Comment(models.Model):
-    body = models.CharField(max_length=500)
-    commented_by = models.ForeignKey(myUser, on_delete= models.CASCADE)   
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at =models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.tagname
 
 class ArtItem(models.Model):
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=500)
     owner = models.ForeignKey(myUser, on_delete=models.CASCADE)
     tags = models.ManyToManyField(Tag)
-    comments = models.ManyToManyField(Comment)
 
-    
-def __str_(self):
-    return self.id
+    def __str__(self):
+        return "Art item: " + self.title
+
+class Comment(models.Model):
+    body = models.CharField(max_length=500)
+    commented_by = models.ForeignKey(myUser, on_delete= models.CASCADE) 
+    commented_on = models.ForeignKey(ArtItem, on_delete= models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at =models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return "A comment made by " + str(self.commented_by) 
+
   
     
 
