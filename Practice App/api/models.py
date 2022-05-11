32d6a90966f19cbd565bc8572d6d40b045d17c2c
Karahan Sarıtaş <44376034+KarahanS@users.noreply.github.com>
2022-05-11 10:25:35 +0300

    added missing fields to the models

--- Practice App/api/models.py
+++ Practice App/api/models.py
@@ -3,19 +3,25 @@ from tkinter import CASCADE
 from django.db import models
 from django.contrib.auth.models import User
 
-
+# Django creates an automatic id field: https://stackoverflow.com/a/35770315/16530078
 
 class myUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     type = models.CharField(max_length=100)  
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at =models.DateTimeField(auto_now=True)
 
 class Tag(models.Model):
     tagname = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at =models.DateTimeField(auto_now=True)
 
 class Comment(models.Model):
     body = models.CharField(max_length=500)
     commented_by = models.ForeignKey(myUser, on_delete= models.CASCADE)   
-
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at =models.DateTimeField(auto_now=True)
+    
 class ArtItem(models.Model):
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=500)
