4da80d6d1fb7baaa688b8a3b44eb23696825335a
Karahan Sarıtaş <44376034+KarahanS@users.noreply.github.com>
2022-05-14 22:50:51 +0300

    added TEST_OUTPUT_DIR to store the test xml outputs in a folder

--- Practice App/django_app/settings.py
+++ Practice App/django_app/settings.py
@@ -129,3 +129,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 TEST_RUNNER = 'xmlrunner.extra.djangotestrunner.XMLTestRunner'
+TEST_OUTPUT_DIR = 'test-output/'
