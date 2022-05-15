3996a91d43624a08cbb5a8b08e14ca667692a1ba
Elif <elif.bayraktar@boun.edu.tr>
2022-05-15 19:23:12 +0300

    Added search_by_tag API, suffix_formatted urlpatterns

--- Practice App/api/urls.py
+++ Practice App/api/urls.py
@@ -7,6 +7,8 @@ Function views
 from django.urls import path
 from .views import views
 #from .views  import view_TVSeries
+from .views import view_search_by_tag
+from rest_framework.urlpatterns import format_suffix_patterns
 
 # practice_home : pattern that matches with the empty route
 # That pattern will be handled by the function views.home(request).
@@ -21,4 +23,10 @@ urlpatterns = [
     path('about/', views.about, name = "about_section"),
     path('api_home/', views.api_home, name= "rest_api"),
     # path('episodes/<str:series>', view_TVSeries.episodes, name="episode_list")
+    path('api/searchbytag/<str:tag>', view_search_by_tag.search_by_tag, name="search_by_tag") #if you're having trouble with tag names make sure it is encoded in the url format. Such as # -> %23. For reference https://www.w3schools.com/tags/ref_urlencode.asp
 ]
+
+#added to give us the option to choose between default Response template and regular json
+#extend the url with <?format=json> to get json
+#if you're having any trouble with your views make sure you added format=None to the arguments
+urlpatterns = format_suffix_patterns(urlpatterns, allowed=['json', 'html'])
---
+++ Practice App/api/views/view_search_by_tag.py
@@ -0,0 +1,24 @@
+import imp
+from unicodedata import name
+from django.http import JsonResponse
+from ..models import ArtItem, Tag #do I need Tag
+from ..serializers import ArtItemSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+
+@api_view(['GET'])
+def search_by_tag(request, tag, format=None):
+
+    try:
+        tagObject = Tag.objects.get(tagname=tag)
+    except Tag.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)    
+
+
+    if request.method == 'GET':
+        items= tagObject.artitem_set.all()
+        # items = ArtItem.objects.all()
+        serializer = ArtItemSerializer(items, many=True)
+        return Response(serializer.data)
