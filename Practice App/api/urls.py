94a029d292731990ef92e1fe77973f6460df242c
Furkan Keskin <keskinfurkan01@gmail.com>
2022-05-16 18:55:30 +0300

    created views for users and added the urls

--- Practice App/api/urls.py
+++ Practice App/api/urls.py
@@ -5,7 +5,7 @@ Function views
 """
 
 from django.urls import path, include
-from .views import view_artitems
+from .views import view_artitems, view_myusers
 from rest_framework.urlpatterns import format_suffix_patterns
 # from .views  import view_TVSeries
 
@@ -22,7 +22,10 @@ urlpatterns = [
     path('api/v1/artitems/', view_artitems.artitems, name="artitems"),
     path('api/v1/artitems/users/<int:id>', view_artitems.artitems_by_userid, name="artitems_by_userid"),
     path('api/v1/artitems/users/<str:username>', view_artitems.artitems_by_username, name="artitems_by_username"),
-    path('api/v1/artitems/<int:id>', view_artitems.artitems_by_id, name="artitem_by_id")
+    path('api/v1/artitems/<int:id>', view_artitems.artitems_by_id, name="artitem_by_id"),
+    path('api/v1/users/', view_myusers.users, name="users"),
+    path('api/v1/users/<int:id>', view_myusers.users_by_id, name="users_by_id"),
+    path('api/v1/users/<str:username>', view_myusers.users_by_username, name="users_by_username")
     # path('api/v1/episodes/<str:series>', view_TVSeries.episodes, name="episode_list")
 ]
 
---
+++ Practice App/api/views/view_myusers.py
@@ -0,0 +1,84 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import JsonResponse 
+from django.forms.models import model_to_dict
+
+from ..models import myUser, Follow, ArtItem, Comment, Tag
+from ..serializers import myUserSerializer, ArtItemSerializer, CommentSerializer, TagSerializer
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.renderers import JSONRenderer
+
+
+from rest_framework.parsers import MultiPartParser
+from rest_framework.decorators import parser_classes
+import json
+
+
+
+##### List of routes / method / action:
+#
+#  http://127.0.0.1:8000/api/v1/users                   /  GET    / Return all of the non-admin users in the system in JSON format 
+#  http://127.0.0.1:8000/api/v1/users/<id>              /  GET    / Return a user with the given id
+#  http://127.0.0.1:8000/api/v1/users/<id>              /  PATCH  / Update a user with the given id
+#  http://127.0.0.1:8000/api/v1/users/<id>              /  DELETE / Delete a user with the given id
+#  http://127.0.0.1:8000/api/v1/users/<username>        /  GET    / Return a user with the given username
+#  http://127.0.0.1:8000/api/v1/users/<username>        /  PATCH  / Update a user with the given username
+#  http://127.0.0.1:8000/api/v1/users/<username>        /  DELETE / Delete a user with the given username
+#
+
+@api_view(["GET"])
+def users(request):
+    myusers = myUser.objects.all()
+    serializer = myUserSerializer(myusers, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+
+@api_view(["GET", "PATCH", "DELETE"])
+def users_by_id(request, id):
+    try:
+        user = myUser.objects.get(pk=id)
+    except myUser.DoesNotExist:
+        return Response({"Not Found:", "Any user with the given ID doesn't exist."}, status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == "GET":
+        serializer = myUserSerializer(user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == "PATCH":
+        serializer = myUserSerializer(user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == "DELETE":
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+@api_view(["GET", "PATCH", "DELETE"])
+def users_by_username(request, username):
+    users = myUser.objects.all()
+    user = [x for x in users if x.username == username]
+
+    if(not user):
+        return Response({"Not Found:", "Any user with the given username doesn't exist."}, status=status.HTTP_404_NOT_FOUND)
+    elif request.method == "GET":
+        serializer = myUserSerializer(user[0])
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == "PATCH":
+        serializer = myUserSerializer(user[0], data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == "DELETE":
+        user[0].delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+
