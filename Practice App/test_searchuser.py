5afac0f8c7c3b93cf526ee5729c4f41be17332a2
Serhat Hebun Şimşek <100690238+simsekhebun@users.noreply.github.com>
2022-05-18 15:32:06 +0300

    adding unit tests
    added a functional unit test for search user

---
+++ Practice App/test_searchuser.py
@@ -0,0 +1,39 @@
+from rest_framework.test import APITestCase
+from api.models import ArtItem, Tag, myUser
+from django.contrib.auth.models import User
+from faker import Faker
+
+class TestSearchUsername(APITestCase):
+    #creating mockup users 
+    def setUp(self):  
+
+        faker = Faker()
+        
+        self.usernames=[]
+        myusers=[[]]*5
+
+        for i in range(5):
+            name=faker.unique.first_name()
+            surname=faker.unique.last_name()
+            email = faker.unique.email()
+            pw=faker.unique.password()
+            self.usernames.append(name)
+            t_user=User.objects.create(username=name,password=pw)
+            myusers[i]=myUser.objects.create(user=t_user,name=name,surname=surname,email=email)
+            
+    
+
+    def tearDown(self):
+        
+        print("TestSearchUsername:setUp_:begin")
+
+        print("TestSearchUsername:setUp_:end")
+
+    def test_search_by_username(self):
+        index=0
+        for _ in self.usernames:
+            response = self.client.get('/api/search_user/{}'.format(self.usernames[index]))
+            self.assertEqual(response.status_code, 200)  # check status code
+            index+=1
+        
+    
