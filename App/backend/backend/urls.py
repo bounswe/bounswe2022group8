12d8b04e1501b8eeac734901b24529ff90b81f5e
Elif <elif.bayraktar@boun.edu.tr>
2022-10-23 11:07:20 +0300

    Created basic homepage with view, added base and index templates.

--- App/backend/backend/urls.py
+++ App/backend/backend/urls.py
@@ -19,6 +19,9 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from . import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name="index"),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
