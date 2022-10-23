8f942c21c5793cf8be1eeff177224abc3773fc86
Karahan Sarıtaş <44376034+KarahanS@users.noreply.github.com>
2022-10-23 12:06:51 +0300

    configure development and production settings

---
+++ App/backend/backend/settings/development.py
@@ -0,0 +1,16 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+ALLOWED_HOSTS = []
+
+DATABASES = {
+'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME':env("DB_NAME"),
+    'USER':env("DB_USER"),
+    'PASSWORD': env("DB_PASSWORD"),
+    'HOST':env("DB_HOST"),
+    'PORT':env("DB_PORT"),
+    }
+}
