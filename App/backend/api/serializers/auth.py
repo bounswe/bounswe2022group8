0276e17707a430452c8a09178972e5e883189d77
Karahan Sarıtaş <44376034+KarahanS@users.noreply.github.com>
2022-10-24 19:32:45 +0300

    Add unittests for username validation

--- App/backend/api/serializers/auth.py
+++ App/backend/api/serializers/auth.py
@@ -5,6 +5,12 @@ from ..models.models import Tag, Comment, ArtItem
 from ..models.user import User
 
 class RegisterSerializer(serializers.ModelSerializer):
+    START_ERROR = "The username must start with a letter."
+    END_ERROR = "The username cannot end with an underscore."
+    ALPHANUM_ERROR = "The username can consist of letters, numbers or underscore."
+    MIN_LENGTH_ERROR = "The username must have at least 6 characters"
+    SUCCESS = ""
+    
     password = serializers.CharField(min_length = 8, write_only=True)
     class Meta:
         model = User
@@ -31,12 +37,12 @@ class RegisterSerializer(serializers.ModelSerializer):
         4) Username must be unique. Don't accept it if it is already present in the database.
         5) It must have at least 6 characters (because reasons)
         """
-        if(not username[0].isalpha()): return (False, "The username must start with a letter.")
-        elif(username[-1] == '_'): return (False, "The username cannot end with an underscore.")
-        elif(not username.replace('_','').isalnum()): return (False, "The username can consist of letters, numbers or underscore.")
-        elif(len(username) < 6): return (False, "The username must have at least 6 characters")
+        if(not username[0].isalpha()): return (False, self.START_ERROR)
+        elif(username[-1] == '_'): return (False, self.END_ERROR)
+        elif(not username.replace('_','').isalnum()): return (False, self.ALPHANUM_ERROR)
+        elif(len(username) < 6): return (False, self.MIN_LENGTH_ERROR)
         # elif(User.objects.filter(username=username) is not None): return (False, "The username must be unique.")
-        else: return (True, "")
+        else: return (True, self.SUCCESS)
 
         # Why did I comment out the last elif? Well, because we defined 'username' field to be unique in the models.user file.
         # Therefore it's not necessary to check it again.
