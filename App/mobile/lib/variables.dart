ab54c1f7421d943cb31c427005342a2c24466440
mustafa-cihan <mustafacihan889@gmail.com>
2022-10-31 14:54:06 +0300

    Added login.dart, register.dart, variables.dart

---
+++ App/mobile/lib/variables.dart
@@ -0,0 +1,7 @@
+
+const String HOST = "127.0.0.1:8000" ;
+const String REGISTER_ENDPOINT = "http://$HOST/api/v1/auth/register/" ;
+const String LOGIN_ENDPOINT = "http://$HOST/api/v1/auth/login/";
+const String LOGOUT_ENDPOINT = "http://$HOST/api/v1/auth/logout/";
+
+String token = "" ;
