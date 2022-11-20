97a8b52224955380724f55ea030fa3a95fc2ccf2
sinemKocoglu <sinemkocoglu560@gmail.com>
2022-11-20 23:45:00 +0300

    added new recommendation pages, improved css

--- App/frontend/src/pages/RecommendedPages/RecommendedArtitems.js
+++ App/frontend/src/pages/RecommendedPages/RecommendedArtitems.js
@@ -2,11 +2,33 @@ import React from "react";
 import Layout from "../../layout/Layout";
 
 import { SampleArtItems } from "../data/SampleArtItems";
+import "../styles/RecommendedArtitems.css";
+//import "../styles/Recommendation.css";
 
 function RecommendedArtitems(props) {
+  function goToArtItem() {
+    //navigate to /artitems/id
+  }
+
   return (
     <Layout>
-
+      <div class="recommendation-container">
+        <div class="recommended-artitems">
+          <div className="gallery">
+            {SampleArtItems.map((val, key) => {
+              return (
+                <div key={key} className="artitem" onClick={() => goToArtItem()}>
+                  <img src={val.src} alt="" />
+                  <div class="context">
+                    <h4>{val.name}</h4>
+                    <p>{val.description}</p>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 }
