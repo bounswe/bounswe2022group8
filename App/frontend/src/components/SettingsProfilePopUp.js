928daa2717af4a833398f8ae849d2946f6405772
Furkan Keskin <keskinfurkan01@gmail.com>
2022-12-04 19:20:12 +0300

    connected the reset password api for logged in users

--- App/frontend/src/components/SettingsProfilePopUp.js
+++ App/frontend/src/components/SettingsProfilePopUp.js
@@ -1,31 +1,31 @@
 import { BsCheckCircle } from "react-icons/bs";
-import "./styles/SettingsPopUp.css";
+import "./styles/SettingsProfilePopUp.css";
 
-function SettingsPopUp(props) {
+function SettingsProfilePopUp(props) {
   return (
-    <div className="settings-pop-up-container">
-      <header className="settings-pop-up-header">
+    <div className="settings-profile-pop-up-container">
+      <header className="settings-profile-pop-up-header">
         <BsCheckCircle
           style={{
             marginBottom: "0.25rem",
             marginRight: "0.5rem",
             fontSize: "28px",
             color: "#198754",
-            strokeWidth: "0.2px",
+            strokeWidth: "0px",
           }}
         />
         Your changes have been saved successfully
       </header>
-      <div className="settings-pop-up-buttons">
+      <div className="settings-profile-pop-up-buttons">
         <button
           onClick={props.onClickProfile}
-          className="btn btn-primary settings-pop-up-btn"
+          className="btn btn-primary settings-profile-pop-up-btn"
         >
           Go to my profile
         </button>
         <button
           onClick={props.onClickStay}
-          className="btn btn-primary settings-pop-up-btn"
+          className="btn btn-primary settings-profile-pop-up-btn"
         >
           Stay
         </button>
@@ -34,4 +34,4 @@ function SettingsPopUp(props) {
   );
 }
 
-export default SettingsPopUp;
+export default SettingsProfilePopUp;
