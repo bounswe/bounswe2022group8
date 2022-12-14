40ea8ba219056fe81b2927d11bacda27a382d2c3
mumcusena <mumcusena.222@gmail.com>
2022-12-14 12:24:14 +0300

    added swagger doc for lexical search view

--- App/backend/api/views/search.py
+++ App/backend/api/views/search.py
@@ -16,6 +16,40 @@ from django.contrib.auth.models import AnonymousUser
 from drf_yasg import openapi
 import base64
 
+#  http://${host}:8000/api/v1/search-lex/  [Method= GET]
+
+@swagger_auto_schema(
+    # method='get',
+    operation_description= "Searchs for art items' title, description, owner name and surname, and tags and sorts according to any of these params.",
+    operation_summary="Get all the art items according to the search filter.",
+    tags=['search'],
+    responses={
+        status.HTTP_200_OK: openapi.Response(
+            description="Successfully retrieved all the art items according to the search params.",
+            examples={
+                "application/json": [
+                    {
+                        "id": 1,
+                        "title": "Flamingos",
+                        "description": "Photo I took during my trip to Manavgat",
+                        "owner": {
+                            "id": 1,
+                            "username": "LenaCaptured",
+                            "name": "Lena",
+                            "surname": "Smith",
+                            "profile_path": "avatar/default.png"
+                        },
+                        "type": "photo",
+                        "tags": [],
+                        "likes": 5,
+                        "artitem_path": "artitem/flamingos.jpg"
+                    }
+                ]
+            }
+        )
+    }
+
+)
 class LexSearchView(ListAPIView):
     queryset = ArtItem.objects.all()
     serializer_class = ArtItemSerializer
