a29354bf2161cadf3f1fdf9f084c71214ee11a22
Karahan Sarıtaş <44376034+KarahanS@users.noreply.github.com>
2022-12-16 22:15:12 +0300

    hotfix: Updated manage.py to bind port 7000 for annotations

--- App/annotations/manage.py
+++ App/annotations/manage.py
@@ -7,6 +7,9 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'annotations.settings.development') # default value is development
+    # Override default port for `runserver` command
+    from django.core.management.commands.runserver import Command as runserver
+    runserver.default_port = "7000"
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
