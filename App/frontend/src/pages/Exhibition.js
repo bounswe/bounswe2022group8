9ded01b8d1ae05535c43db6d1e7b0a5cab8d3443
Furkan Keskin <keskinfurkan01@gmail.com>
2022-12-25 21:27:32 +0300

    initial version of the online exhibition page has been created

---
+++ App/frontend/src/pages/Exhibition.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { useAuth } from "../auth/authentication";
+import { HOST } from "../constants/host";
+import Layout from "../layout/Layout";
+import * as dotenv from "dotenv";
+
+import { SampleGallery } from "./data/SampleGallery";
+
+import "./styles/Exhibition.css";
+
+function Exhibition(props) {
+  function scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "instant",
+    });
+  }
+
+  var host = HOST;
+  const { token } = useAuth();
+
+  const [focusIdx, setFocusIdx] = useState(0);
+  const [focusSrc, setFocusSrc] = useState(SampleGallery[0].src);
+
+  function handleFocus(src, index) {
+    setFocusIdx(index);
+    setFocusSrc(src);
+  }
+
+  return (
+    <Layout>
+      <div className="exhibition-post-container">
+        <div className="exhibition-post">
+          <div className="exhibition-container">
+            <img className="artitem-focused" src={focusSrc} alt={""} />
+            <div className="exhibition-linear-container">
+              {SampleGallery.map((val, index) => {
+                return (
+                  <div
+                    className="exhibition-linear-image-wrap"
+                    onClick={() => handleFocus(val.src, index)}
+                    style={{
+                      border: index === focusIdx ? "2px solid white" : "none",
+                    }}
+                  >
+                    <img
+                      className="exhibition-linear-image"
+                      src={val.src}
+                      alt=""
+                    />
+                    <div className="exhibition-linear-image-cover"></div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+
+          <div className="exhibition-info-container">
+            <div className="exhibition-owner">
+              <img className="exhibition-owner-profile-photo" src={""} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+}
+
+export default Exhibition;
