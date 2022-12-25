f374357ac60156c1e41dcff20f64e2ba67939ada
MustafaEmreErengul <m.erengul.52@gmail.com>
2022-12-25 21:45:52 +0300

    self_profile.dart is created
    Visiting own profile and visiting others profile now have different pages.

--- App/mobile/lib/profile_page.dart
+++ App/mobile/lib/profile_page.dart
@@ -4,7 +4,7 @@ import 'package:flutter/services.dart';
 import 'package:artopia/home_page.dart';
 import 'package:artopia/landing_page.dart';
 import 'profile.dart';
-import 'package:artopia/widgets/profile_header_widgets.dart';
+import 'package:artopia/widgets/self_profile.dart';
 import 'package:artopia/utils/colorPalette.dart';
 import 'package:artopia/utils/textUtils.dart';
 import 'package:artopia/settings_page.dart';
@@ -12,7 +12,8 @@ import 'package:artopia/art_items_tab.dart';
 import 'package:artopia/exhibitions_tab.dart';
 import 'package:flutter/cupertino.dart';
 import 'dart:core';
-import 'package:image_picker/image_picker.dart';
+import 'package:avatar_glow/avatar_glow.dart';
+import 'package:dotted_border/dotted_border.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({Key? key}) : super(key: key);
@@ -24,7 +25,17 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   final ColorPalette colorPalette = ColorPalette();
   final textUtils = TextUtils();
-  Future<Profile> myProfile = getMyProfile() ;  
+  Future<Profile> myProfile = getMyProfile() ;
+  List<bool> isSelected = [true, false];
+  bool isButtonPressed = false;
+  int flag = 0;
+
+  @override
+  void initState() {
+    isSelected = [true, false];
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -75,7 +86,8 @@ class _ProfilePageState extends State<ProfilePage> {
               SliverList(
                 delegate: SliverChildListDelegate(
                   [
-                   profileHeaderWidget(context,me),
+                    selfProfile(context,me),
+                    //otherProfile(context,me),
                   ],
                 ),
               ),
@@ -157,6 +169,225 @@ class _ProfilePageState extends State<ProfilePage> {
             label: 'Logout',
           ),
         ],
+      ),
+    );
+  }
+
+  otherProfile(BuildContext context, Profile user) {
+
+    return Container(
+
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: colorPalette.graniteGray,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(left: 18.0, right: 18.0, bottom: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(padding: const EdgeInsets.only(left: 5)),
+                Column(
+                  children: [
+                    SizedBox(
+                      width: 120,
+                      height: 120,
+                      child: AvatarGlow(
+                        glowColor: colorPalette.darkPurple,
+                        endRadius: 90.0,
+                        duration: Duration(milliseconds: 2000),
+                        repeat: true,
+                        showTwoGlows: true,
+                        repeatPauseDuration: Duration(milliseconds: 100),
+                        child: DottedBorder(
+                          radius: Radius.circular(5),
+                          color: colorPalette.darkPurple,
+                          strokeWidth: 8,
+                          borderType: BorderType.Circle,
+                          dashPattern: [1, 12],
+                          strokeCap: StrokeCap.butt,
+                          child: Center(
+                            child: SizedBox(
+                              width: 130,
+                              height: 130,
+                              child: CircleAvatar(
+
+
+                                foregroundImage: Image
+                                    .network(
+                                    user.imageUrl)
+
+                                    .image,
+                                radius: 10,
+
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Padding(padding: const EdgeInsets.only(left: 20)),
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: 20),
+                    Row(
+                      children: [
+                        textUtils.buildText(
+                            user.username, 18, Colors.white70, FontWeight.w500
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 5),
+                    Row(
+                      children: [
+                        textUtils.buildText(
+                            user.name, 13, Colors.white70, FontWeight.w500
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 15),
+                    Container(
+                      //padding: const EdgeInsets.all(16.0),
+                      width: 200,
+                      //width: MediaQuery.of(context).size.width*0.8,
+                      child: textUtils.buildText(
+                          user.bio, 12, Colors.white70, FontWeight.w500
+                      ),
+                    ),
+
+
+                    SizedBox(height: 15),
+                    Row(
+                      children: [
+                        Wrap(
+                          crossAxisAlignment: WrapCrossAlignment.center,
+                          children: [
+                            Icon(Icons.location_on),
+                            textUtils.buildText(
+                                user.location, 12, Colors.white70, FontWeight.w500
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 10),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        GestureDetector(
+                          child: textUtils.buildText(
+                              "Followers ", 12, Colors.black87, FontWeight.w500
+                          ),
+                          onTap: () =>
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) =>
+                                    HomePage()),
+                              ),
+                        ),
+                        textUtils.buildText(
+                            user.followers.toString(), 13, Colors.black,
+                            FontWeight.w500
+                        ),
+                        SizedBox(width: 20),
+                        GestureDetector(
+
+                          child:
+                          textUtils.buildText(
+                              " Following ", 12, Colors.black87, FontWeight.w500
+                          ),
+                          onTap: () =>
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) =>
+                                    HomePage()),
+                              ),
+                        ),
+                        textUtils.buildText(
+                            user.following.toString(), 13, Colors.black,
+                            FontWeight.w500
+                        ),
+                      ],
+                    ),
+                  ],
+                )
+              ],
+            ),
+            const Padding(padding: const EdgeInsets.only(top: 10)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left: 10),
+                  child:
+                      Container(
+                        width: 100,
+                        height: 30,
+                        child:
+                        ElevatedButton(
+                          onPressed: () async {
+                            setState(() {
+                              if(isButtonPressed == false){
+                                if(flag == 0){
+                                  user.followers = user.followers + 1;
+                                  flag = flag + 1;
+                                }
+                              }
+                              else{
+                                if(flag > 0){
+                                  user.followers = user.followers - 1;
+                                  flag= 0;
+                                }
+                              }
+                              isButtonPressed = !isButtonPressed;
+                            });
+                          },
+                          style: isButtonPressed ? ElevatedButton.styleFrom(//pressed
+                            backgroundColor: colorPalette.frenchLilac,
+                            foregroundColor: Colors.black,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                          ) :
+                          ElevatedButton.styleFrom(//not_pressed
+                            backgroundColor: colorPalette.blackShadows,
+                            foregroundColor: Colors.black,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                          ),
+                          child:
+                          Container(
+                            height: 30,
+                            width: 65,
+                            child: Center(
+                                child: isButtonPressed ? textUtils.buildText(
+                                    "Following", 13, Colors.white, FontWeight.w500
+                                ) :
+                                textUtils.buildText(
+                                    "Follow", 13, Colors.white, FontWeight.w500
+                                )
+                            ),
+                          ),
+                        ),
+                      ),
+
+
+
+
+
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ App/mobile/lib/widgets/self_profile.dart
@@ -0,0 +1,188 @@
+import 'package:artopia/art_item_upload.dart';
+import 'package:artopia/home_page.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:artopia/profile.dart';
+import 'dart:core';
+import 'package:avatar_glow/avatar_glow.dart';
+import 'package:dotted_border/dotted_border.dart';
+import 'package:artopia/utils/colorPalette.dart';
+import 'package:artopia/utils/textUtils.dart';
+
+
+
+Widget selfProfile(BuildContext context, Profile me) {
+  final ColorPalette colorPalette = ColorPalette();
+  final textUtils = TextUtils();
+
+
+  return Container(
+    width: double.infinity,
+    decoration: BoxDecoration(
+      color: colorPalette.graniteGray,
+    ),
+    child: Padding(
+      padding: const EdgeInsets.only(left: 18.0, right: 18.0, bottom: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Padding(padding: const EdgeInsets.only(left: 5)),
+              Column(
+                children: [
+                  SizedBox(
+                    width: 120,
+                    height: 120,
+                    child: AvatarGlow(
+                      glowColor: colorPalette.darkPurple,
+                      endRadius: 90.0,
+                      duration: Duration(milliseconds: 2000),
+                      repeat: true,
+                      showTwoGlows: true,
+                      repeatPauseDuration: Duration(milliseconds: 100),
+                      child: DottedBorder(
+                        radius: Radius.circular(5),
+                        color: colorPalette.darkPurple,
+                        strokeWidth: 8,
+                        borderType: BorderType.Circle,
+                        dashPattern: [1, 12],
+                        strokeCap: StrokeCap.butt,
+                        child: Center(
+                          child: SizedBox(
+                            width: 130,
+                            height: 130,
+                            child: CircleAvatar(
+
+                              foregroundImage: Image.network(
+                                  me.imageUrl)
+
+                                  .image,
+                              radius: 10,
+
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Padding(padding: const EdgeInsets.only(left: 20)),
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: 20),
+                  Row(
+                    children: [
+                      textUtils.buildText(
+                          me.username, 18, Colors.white70, FontWeight.w500
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 5),
+                  Row(
+                    children: [
+                      textUtils.buildText(
+                          me.name, 13, Colors.white70, FontWeight.w500
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 15),
+                  Container (
+                    //padding: const EdgeInsets.all(16.0),
+                    width: 200,
+                    //width: MediaQuery.of(context).size.width*0.8,
+                    child: textUtils.buildText(
+                        me.bio, 12, Colors.white70, FontWeight.w500
+                    ),
+                  ),
+
+
+                  SizedBox(height: 15),
+                  Row(
+                    children: [
+                      Wrap(
+                        crossAxisAlignment: WrapCrossAlignment.center,
+                        children: [
+                          Icon(Icons.location_on),
+                          textUtils.buildText(
+                              me.location, 12, Colors.white70, FontWeight.w500
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      GestureDetector(
+                        child: textUtils.buildText(
+                            "Followers ", 12, Colors.black87, FontWeight.w500
+                        ),
+                        onTap: () => Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => HomePage()),
+                        ),
+                      ),
+                      textUtils.buildText(
+                          me.followers.toString(), 13, Colors.black, FontWeight.w500
+                      ),
+                      SizedBox(width: 20),
+                      GestureDetector(
+
+                        child:
+                        textUtils.buildText(
+                            " Following ", 12, Colors.black87, FontWeight.w500
+                        ),
+                        onTap: () => Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => HomePage()),
+                        ),
+                      ),
+                      textUtils.buildText(
+                          me.following.toString(), 13, Colors.black, FontWeight.w500
+                      ),
+                    ],
+                  ),
+                ],
+              )
+            ],
+          ),
+          const Padding(padding: const EdgeInsets.only(top: 10)),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(right: 20),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: colorPalette.blackShadows,
+                    foregroundColor: Colors.black,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                  onPressed: () => {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => UploadArtItem()
+                      ),
+                    ),
+                  },
+                  child: textUtils.buildText(
+                      "Add Art Item", 13, Colors.black, FontWeight.w500
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    ),
+  );
+}
